01d4db9d79224eb1de54c8e57180fed075d224c6
Christos Paraskevopoulos <christos1129@gmail.com>
2025-10-24 14:28:11 +0100

    Initial commit - PingMonitor project

--- FolderMonitor/run_app.vbs
+++
@@ -1,2 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run """" & WScript.ScriptFullName & "\..\foldermonitor.exe" & """", 0, False
--- FolderMonitor/uninstall.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set TASK_NAME=FolderMonitor
-
-echo Removing tasks...
-schtasks /delete /f /tn "%TASK_NAME%"
-schtasks /delete /f /tn "%TASK_NAME% - Startup"
-
-echo Uninstalled.
-pause
